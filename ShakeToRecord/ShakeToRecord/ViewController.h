121f5e79ab264c63d4524e4c66f2391ef7deb83d
Xoi Hin <xoihin@yahoo.com>
2015-10-13 20:35:09 -0400

    Init...

---
+++ ShakeToRecord/ShakeToRecord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShakeToRecord
+//
+//  Created by Xoi's iMac on 2015-10-13.
+//  Copyright (c) 2015 XoiAHin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShakeToRecord/ShakeToRecord/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShakeToRecord
+//
+//  Created by Xoi's iMac on 2015-10-13.
+//  Copyright (c) 2015 XoiAHin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
